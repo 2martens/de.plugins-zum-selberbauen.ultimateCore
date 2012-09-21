f21e3792d8507dd4dd94dcb46926c9dc16d958c3
Jim Martens <jim1@live.de>
2012-09-21 12:42:36 +0200

    Added user, comment and comment response cache. Added new dependency.

---
+++ src/lib/system/cache/builder/CommentResponseCacheBuilder.class.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Contains the CommentResponseCacheBuilder class.
+ * 
+ * LICENSE:
+ * This file is part of the Ultimate Core.
+ *
+ * The Ultimate Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The Ultimate Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Ultimate Core.  If not, see {@link http://www.gnu.org/licenses/}.
+ * 
+ * @author		Jim Martens
+ * @copyright	2011-2012 Jim Martens
+ * @license		http://www.gnu.org/licenses/lgpl-3.0 GNU Lesser General Public License, version 3
+ * @package		de.plugins-zum-selberbauen.ultimateCore
+ * @subpackage	system.cache.builder
+ * @category	Community Framework
+ */
+namespace wcf\system\cache\builder;
+use wcf\data\comment\response\CommentResponseList;
+
+/**
+ * Caches the comment responses.
+ * 
+ * Provides two variables:
+ * * \wcf\data\comment\response\CommentResponse[] responses
+ * * integer[] responseIDs
+ * 
+ * @author		Jim Martens
+ * @copyright	2011-2012 Jim Martens
+ * @license		http://www.gnu.org/licenses/lgpl-3.0 GNU Lesser General Public License, version 3
+ * @package		de.plugins-zum-selberbauen.ultimateCore
+ * @subpackage	system.cache.builder
+ * @category	Community Framework
+ */
+class CommentResponseCacheBuilder implements ICacheBuilder {
+	/**
+	 * @see \wcf\system\cache\builder\ICacheBuilder::getData()
+	 */
+	public function getData(array $cacheResource) {
+		$data = array(
+			'responses' => array(),
+			'responseIDs' => array()
+		);
+		
+		$responseList = new CommentResponseList();
+		$responseList->readObjects();
+		$responses = $responseList->getObjects();
+		$responseIDs = $responseList->getObjectIDs();
+		
+		if (empty($reponses)) return $data;
+		
+		$data['responses'] = $responses;
+		$data['responseIDs'] = $responseIDs;
+		
+		return $data;
+	}
+}
